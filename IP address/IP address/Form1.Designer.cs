76d0e7ccb1a84d54cdf86b0d5d8952abe02f57fd
chorroO <zhenia.copeikin@yandex.ru>
2017-06-29 09:43:50 +0300

    Add files via upload

---
+++ IP address/IP address/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace IP_address
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(150, 537);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(193, 33);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 4;
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox_MaskInputRejected);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(193, 59);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox2.TabIndex = 5;
+            this.maskedTextBox2.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox_MaskInputRejected);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Введите диапазон";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(172, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "от";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(172, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "до";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 561);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Search IP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ IP address/IP address/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IP_address
+{
+    public partial class Form1 : Form
+    {
+        List<string> allIP = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            maskedTextBox1.Mask = @"999\.999\.999\.999";
+            maskedTextBox2.Mask = @"999\.999\.999\.999";
+            maskedTextBox1.PromptChar = ' ';
+            maskedTextBox2.PromptChar = ' ';
+            maskedTextBox1.MaskInputRejected += new MaskInputRejectedEventHandler(maskedTextBox_MaskInputRejected);
+            maskedTextBox2.MaskInputRejected += new MaskInputRejectedEventHandler(maskedTextBox_MaskInputRejected);
+
+            openFile();
+        }
+
+        private void maskedTextBox_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            toolTip1.ToolTipTitle = "Ошибка ввода";
+            toolTip1.Show("Допустимые значения: (0-9)", maskedTextBox2, 0, 30, 3000);
+        }
+
+        private void searchIP(string startIP, string endIP, List<string> allIP)
+        {
+            string[] splitStartrIP = startIP.Split('.');
+            string[] splitEndIP = endIP.Split('.');
+            string[] splitAllIP;
+            int[] start = new int[4];
+            int[] end = new int[4];
+            int[] all = new int [4];
+            bool b = true;
+
+            string sSIP, sEIP;
+
+            for (int i = 0; i < 4; i++)
+            {
+                sSIP = splitStartrIP[i].Replace(" ", string.Empty);
+                sEIP = splitEndIP[i].Replace(" ", string.Empty);
+
+                if (sSIP == string.Empty) sSIP = "0";
+                if (sEIP == string.Empty) sEIP = "255";
+
+                start[i] = Convert.ToInt32(sSIP);
+                end[i] = Convert.ToInt32(sEIP);
+
+                if (start[i] > end[i] || start[i] < 0 || end[i] > 255)
+                {
+                    toolTip1.ToolTipTitle = "Ошибка ввода";
+                    toolTip1.Show("Значение 'от' не может превышать значение 'до' " +
+                        "\n Значения 'от' не могут быть меньше 0" +
+                        "\n Значения 'до' не могут быть больше 255", maskedTextBox2, 0, 30, 3000);
+
+                    return;
+                }
+            }
+
+            foreach (var ip in allIP)
+            {
+                splitAllIP = ip.Split('.');
+
+                b = true;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    all[i] = Convert.ToInt32(splitAllIP[i]);
+
+                    if (all[i] < start[i] || all[i] > end[i])
+                    {
+                        b = false;
+                        break;
+                    }
+                }
+
+                if (b) richTextBox1.Text += ip + "\n";
+            }
+        }
+
+        private void openFile()
+        {
+            string line = string.Empty;
+
+            try
+            {   
+                using (StreamReader sr = new StreamReader("IP.txt"))
+                {
+                    while ((line = sr.ReadLine()) != null) allIP.Add(line);
+                    sr.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            searchIP(maskedTextBox1.Text, maskedTextBox2.Text, allIP);
+        }
+    }
+}
